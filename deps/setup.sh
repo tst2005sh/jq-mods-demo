0f5f1d1bc9f5f7d3e59f8693a401d3562f7dd404
TsT <tst2005@gmail.com>
2022-04-19 18:25:36 +0200

    deps

---
+++ deps/setup.sh
@@ -0,0 +1,8 @@
+set -e
+cd "$(dirname "$0")/../deps/"
+
+[ -e "jq-helpers" ] ||
+git clone https://github.com/tst2005sh/jq-helpers
+
+[ -e "jq-mods" ] ||
+git clone https://github.com/tst2005sh/jq-mods
