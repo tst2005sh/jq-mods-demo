094376b5f7e98490bd82f4a28c4b3e482cdc140a
TsT <tst2005@gmail.com>
2022-07-28 16:10:50 +0200

    more test for frombase/tobase

--- demos/frombase.sh
+++ demos/frombase.sh
@@ -2,5 +2,13 @@
 . ./deps/jq-helpers/lib/jq_stack4.lib.sh
 JQ_STACK4_MODDIR=./lib
 jq_stack4 -ncM :modload frombase :call '
+(
 	[0,1,1,1] | frombase(2) == 14
+),(
+	[10,0] | frombase(10) == 10
+),(
+	[0,10] | frombase(10) == 100
+),(
+	[25,25] | frombase(26) == 650
+)
 ' :run
--- demos/tobase.sh
+++ demos/tobase.sh
@@ -6,5 +6,9 @@ jq_stack4 -ncM :modload tobase :call '
 	14 | tobase(2)== [0,1,1,1]
 ),(
 	0 | tobase(2)== [0]
+),(
+	(63| tobase(32)) == [31,1],
+	(64| tobase(32)) == [0,2],
+	(65| tobase(32)) == [1,2]
 )
 ' :run
