73142b8ecc4a05d851da834d84426e27dd64022d
TsT <tst2005@gmail.com>
2022-07-29 18:29:57 +0200

    arrayseparator

--- demos/arrayseparator.sh
+++ demos/arrayseparator.sh
@@ -1,12 +1,15 @@
-. ./lib/arrayseparator.jq.lib.sh
+
+
+. ./deps/jq-helpers/lib/jq_stack4.lib.sh
+JQ_STACK4_MODDIR=./lib
 
 input='["a","b","c"]'
 expected='["a","_","b","_","c"]'
 result="$(
-	echo "$input" |	
-	jq -cM "$jq_function_arrayseparator"'
+	echo "$input" |
+	jq_stack4 -cM :modload arrayseparator :call '
 		arrayseparator("_")
-	'
+	' :run
 )"
 [ "$result" = "$expected" ] && echo ok || echo KO
 
@@ -15,10 +18,10 @@ input='"aa,b b,cc"'
 expected='"(aa),(b b),(cc)"'
 result="$(
 	echo "$input" |	
-	jq -cM "$jq_function_arrayseparator"'
+	jq_stack4 -cM :modload arrayseparator :call '
 		split(",")
 		| arrayseparator("),(")
 		| "(\(join("")))"
-	'
+	' :run
 )"
 [ "$result" = "$expected" ] && echo ok || echo KO
