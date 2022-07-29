dceca44fed17cdbc041dff1135bf80200e6ca9b9
TsT <tst2005@gmail.com>
2022-07-29 18:34:49 +0200

    array_to_object

--- demos/array_to_object.sh
+++ demos/array_to_object.sh
@@ -1,10 +1,10 @@
 
-. ./lib/array_to_object.jq.lib.sh
+. ./deps/jq-helpers/lib/jq_stack4.lib.sh
+JQ_STACK4_MODDIR=./lib
 
 result="$(
 	echo '[["BUZ","BAR","FOO"],["zzz1","bar1","foo1"],[null,"bar2","foo2"]]' |
-	jq -cM "$jq_function_array_to_object"'array_to_object'
+	jq_stack4 -cM :modcall array_to_object :run
 )"
-
 expected='[{"BUZ":"zzz1","BAR":"bar1","FOO":"foo1"},{"BUZ":null,"BAR":"bar2","FOO":"foo2"}]'
 [ "$result" = "$expected" ] && echo ok || echo ko
