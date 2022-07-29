14e1d4b53946797d5b1c2fd6a75e36f37771b6b6
TsT <tst2005@gmail.com>
2022-07-29 18:50:28 +0200

    fromtsv

--- demos/fromtsv.sh
+++ demos/fromtsv.sh
@@ -1,25 +1,28 @@
-. ./lib/fromtsv.jq.lib.sh
+
+. ./deps/jq-helpers/lib/jq_stack4.lib.sh
+JQ_STACK4_MODDIR=./lib
 
 #         [["X","XX","XXX","XXXX"],["a'a","b\"b","\"c'c\"","\"d\"d\""]]
 expected='[["X","XX","XXX","XXXX"],["a'\''a","b\"b","\"c'\''c\"","\"d\"d\""]]'
 
 result="$(
 	cat data-samples/sample1a.tsv |
-	jq -RcMr "$jq_function_fromtsv"' [.,inputs]|fromtsv'
+	jq_stack4 -RcMr :modload fromtsv :call '[.,inputs]|fromtsv' :run
+
 )"
 [ "$result" = "$expected" ] && echo ok || echo ko
 
 result="$(
 	cat data-samples/sample1b.tsv |
-	jq -RcMr "$jq_function_fromtsv"' [.,inputs]|fromtsv'
+	jq_stack4 -RcMr :modload fromtsv :call '[.,inputs]|fromtsv' :run
 )"
 [ "$result" = "$expected" ] && echo ok || echo ko
 
 result="$(
 	printf 'foo\tbar\n"x\\ny"\tBAR\n' |
-	jq -RcMr "$jq_function_fromtsv"'
+	jq_stack4 -RcMr :modload fromtsv :call '
 		[.,inputs]|fromtsv == [["foo","bar"],["x\ny","BAR"]]
-	'
+	' :run
 )"
 expected='true'
 [ "$result" = "$expected" ] && echo ok || echo ko
