0f1e78502fec98c017116927419e0fc097ed456c
TsT <tst2005@gmail.com>
2025-03-20 15:54:38 +0100

    add: flat

---
+++ demos/flat.sh
@@ -0,0 +1,13 @@
+
+. ./deps/jq-helpers/lib/jq_stack4.lib.sh
+JQ_STACK4_MODDIR=./lib
+
+echo '
+{"id":1,"foo":"foo",                "custom_fields":{"x":"x1","foo":"FOO1"}}
+{"id":2,"foo":"foo2", "bar":"bar2" ,"custom_fields":{"x":"x2","foo":"FOO2"}}
+{"id":3,"foo":"foo3", "bar":"bar3" ,"custom_fields":null}
+{"id":4,"foo":"foo4", "bar":"bar4"}
+' |
+jq_stack4 -cM :modload flat :call '
+flat(if first=="custom_fields" then "CF_\(.[1])" elif length==1 then first else join("/") end)
+' :run
