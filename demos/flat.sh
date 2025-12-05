99b6e52d4cd68b7108122a0812e7c27c2823d916
TsT <tst2005@gmail.com>
2025-12-05 16:55:21 +0100

    better flat demo

--- demos/flat.sh
+++ demos/flat.sh
@@ -11,3 +11,73 @@ echo '
 jq_stack4 -cM :modload flat :call '
 flat(if first=="custom_fields" then "CF_\(.[1])" elif length==1 then first else join("/") end)
 ' :run
+
+echo '
+{
+  "YYYY-MM": {
+    "A": {
+      "FAILURE": {
+        "count": 12,
+        "expected_total_of_success_and_failure": 8928
+      },
+      "SUCCESS": {
+        "count": 123,
+        "expected_total_of_success_and_failure": 8928
+      }
+    },
+    "B": {
+      "FAILURE": {
+        "count": 456,
+        "expected_total_of_success_and_failure": 8928
+      },
+      "SUCCESS": {
+        "count": 78,
+        "expected_total_of_success_and_failure": 8928
+      }
+    },
+    "C": {
+      "FAILURE": {
+        "count": 333,
+        "expected_total_of_success_and_failure": 44640
+      },
+      "SUCCESS": {
+        "count": 444,
+        "expected_total_of_success_and_failure": 44640
+      }
+    },
+    "D": {
+      "FAILURE": {
+        "count": 23,
+        "expected_total_of_success_and_failure": 44640
+      },
+      "SUCCESS": {
+        "count": 1023,
+        "expected_total_of_success_and_failure": 44640
+      }
+    },
+    "E": {
+      "FAILURE": {
+        "count": 757,
+        "expected_total_of_success_and_failure": 44640
+      },
+      "SUCCESS": {
+        "count": 987,
+        "expected_total_of_success_and_failure": 44640
+      }
+    },
+    "F": {
+      "FAILURE": {
+        "count": 700,
+        "expected_total_of_success_and_failure": 44640
+      },
+      "SUCCESS": {
+        "count": 300,
+        "expected_total_of_success_and_failure": 44640
+      }
+    }
+  }
+}
+' |
+jq_stack4 -cM :modload flat :call '
+flat(join("/"))
+' :run
